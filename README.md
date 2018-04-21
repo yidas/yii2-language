9f8187c49ae4d5fa3123597dcfe0a155fae6cad5
Nick Tsai <myintaer@gmail.com>
2018-04-21 16:20:04 +0800

    Release version 1.0.0

--- README.md
+++ README.md
@@ -1,2 +1,127 @@
-# yii2-language
-Language component for Yii 2 framework
+<p align="center">
+    <a href="https://github.com/yiisoft" target="_blank">
+        <img src="https://avatars0.githubusercontent.com/u/993323" height="100px">
+    </a>
+    <h1 align="center">Yii2 Language</h1>
+    <br>
+</p>
+
+Language extension component for Yii 2 framework.
+
+[![Latest Stable Version](https://poser.pugx.org/yidas/yii2-language/v/stable?format=flat-square)](https://packagist.org/packages/yidas/yii2-language)
+[![Latest Unstable Version](https://poser.pugx.org/yidas/yii2-language/v/unstable?format=flat-square)](https://packagist.org/packages/yidas/yii2-language)
+[![License](https://poser.pugx.org/yidas/yii2-language/license?format=flat-square)](https://packagist.org/packages/yidas/yii2-language)
+
+FEATURES
+--------
+
+- ***language Mapping** integrated with Yii2 Language*
+
+
+---
+
+REQUIREMENTS
+------------
+
+This library requires the following:
+
+- PHP 5.4.0+
+- Yii 2.0.0+
+
+---
+
+
+INSTALLATION
+------------
+
+Install via Composer in your Yii2 project:
+
+```
+composer require yidas/yii2-language
+```
+
+---
+
+CONFIGURATION
+-------------
+
+Add a component using `yidas\components\Language` with configurations:
+
+```php
+return [
+    'bootstrap' => ['log', 'lang'],
+    'components' => [
+        'lang' => [
+            'class' => 'yidas\components\Language',
+            'languages' => [
+                0 => 'en-US',
+                1 => 'zh-TW',
+                2 => 'zh-CN',
+            ],
+            'maps' => [
+                'html' => [
+                    0 => 'en',
+                    1 => 'zh-Hant',
+                    2 => 'zh-Hans',
+                ],
+            ],
+            // 'storage' => 'session',
+            // 'storageKey' => 'language',
+        ],
+        ...
+```
+
+|property|Description|
+|:-|:-|
+|languages|Supported language list|
+|maps|Customized language map|
+|storage|Storage carrier such as Session and Cookie|
+|storageKey|Storage carrier Key|
+
+### Bootstrap
+
+You could add the language component into `bootstrap` for keeping the language storage work such as Seesion and Cookie.
+
+```php
+// `lang` component for example
+return [
+    'bootstrap' => ['lang'], 
+    ...
+```
+
+---
+
+USAGE
+-----
+
+### `get()`
+
+Get Current Language
+
+```php
+echo \Yii::$app->lang->get();  // en-US
+```
+
+You could get from map by giving map key as first argument:
+
+```php
+echo \Yii::$app->lang->get('html');  // en
+```
+
+
+
+### `getByMap()`
+
+Get customized language value from $map
+
+```php
+echo \Yii::$app->lang->getByMap('html');  // en
+```
+
+### `set()`
+
+Set Current Language
+
+```php
+\Yii::$app->lang->set('zh-TW');
+```
---
+++ src/Language.php
@@ -0,0 +1,251 @@
+<?php
+
+namespace yidas\components;
+
+use Yii;
+use yii\base\Component;
+use yii\base\InvalidConfigException;
+
+/**
+ * Language Extension Component
+ * 
+ * @author  Nick Tsai <myintaer@gmail.com>
+ * @version 1.0.0
+ */
+class Language extends Component
+{
+    /**
+     * Supported language list
+     *
+     * @var array Values refer to \Yii::$app->language
+     */
+    public $languages = [
+        0 => 'en-US',
+        1 => 'zh-TW',
+        2 => 'zh-CN',
+    ];
+
+    /**
+     * Customized language map
+     *
+     * @var array Values map to $languages
+     * @example $this->getByMap('html');
+     */
+    public $maps = [
+        'html' => [
+            0 => 'en',
+            1 => 'zh-Hant',
+            2 => 'zh-Hans',
+        ],
+    ];
+
+    /**
+     * Storage carrier such as Session and Cookie
+     *
+     * @var string ['session', 'cookie']
+     */
+    public $storage = 'session';
+
+    /**
+     * Storage carrier Key
+     *
+     * @var string
+     */
+    public $storageKey = 'language';
+
+    /**
+     * Current Language
+     *
+     * @var string Should same as \Yii::$app->language
+     */
+    private $_currentLang = null;
+
+    /**
+     * Current Language index
+     *
+     * @var integer
+     */
+    private $_currentLangIndex = null;
+
+    /**
+     * Bootstrap
+     * 
+     * You could register this componet then add to 'bootstrap' in configuration.
+     *
+     * @return void
+     */
+    public function init()
+    {
+        // Get Language from storage such as Session or Cookie
+        $language = $this->_getFromStorage();
+
+        if ($language) {
+            // Set to current language
+            $this->set($language);
+        } else {
+            // Refer Yii::$app->language as default language
+            if (Yii::$app->language) {
+                $this->set(Yii::$app->language);
+            } else {
+                $this->setByIndex(0);
+            } 
+        }
+    }
+
+    /**
+     * Get Current Language
+     *
+     * @return string
+     */
+    public function get($map=null)
+    {
+        if ($map) {
+            // Get from map
+            return $this->getByMap($map);
+        }
+        
+        return ($this->_currentLang) ?: Yii::$app->language;
+    }
+
+    /**
+     * Get Current Language index
+     *
+     * @return integer
+     */
+    public function getIndex()
+    {
+        return $this->_currentLangIndex;
+    }
+
+    /**
+     * Get customized language value from $map
+     *
+     * @param string $map Variable name defined in component
+     * @return string Language value from diving $map with current language
+     * @example $this->getByMap('htmlMap');
+     */
+    public function getByMap($mapKey)
+    {
+        $index = $this->getIndex();
+
+        if (!isset($this->maps[$mapKey])) {
+            
+            throw new Exception("Given map `{$mapKey}` doesn't declared in Language Component", 500);
+        }
+
+        return isset($this->maps[$mapKey][$index]) ? $this->maps[$mapKey][$index] : null;
+    }
+
+    /**
+     * Set Current Language
+     *
+     * @param string $language Refer to $languages
+     * @return bool Result
+     */
+    public function set($language)
+    {
+        $index = array_search($language, $this->languages);
+
+        // Check the match from supported language list 
+        if ($index === false) {
+            // Not matched
+            return false;
+
+        } else {
+            // Set status
+            $this->_set($index, $language);
+            // Success
+            return true;
+        }
+    }
+
+    /**
+     * Set Current Language
+     *
+     * @param string $language Refer to $languages
+     * @return bool Result
+     */
+    public function setByIndex($index)
+    {
+        $language = isset($this->languages[$index]) ? $this->languages[$index] : false;
+
+        // Check the match from supported language list 
+        if ($index === false) {
+            // Not matched
+            return false;
+
+        } else {
+            // Set status
+            $this->_set($index, $language);
+            // Success
+            return true;
+        }
+    }
+
+    /**
+     * Set Language
+     *
+     * @param integer $index
+     * @param string $lang
+     * @return void
+     */
+    protected function _set($index, $language)
+    {
+        $this->_currentLang = $language;
+        $this->_currentLangIndex = $index;
+        // Storage
+        $this->_setToStorage($language);
+        // Yii Language
+        Yii::$app->language = $language;
+    }
+    
+    /**
+     * Get current language from storage
+     *
+     * @return string Language
+     */
+    protected function _getFromStorage()
+    {
+        switch ($this->storage) {
+
+            case 'session':
+                return Yii::$app->session->get($this->storageKey);
+                break;
+            
+            case 'cookie':
+                return Yii::$app->request->cookies->getValue($this->storageKey);
+                break;
+            
+            default:
+                return false;
+                break;
+        }
+    }
+
+    /**
+     * Set language to storage
+     *
+     * @param string $language Refer to $languages
+     * @return bool Result
+     */
+    protected function _setToStorage($language)
+    {
+        switch ($this->storage) {
+
+            case 'session':
+                return Yii::$app->session->set($this->storageKey, $language);
+                break;
+            
+            case 'cookie':
+                return Yii::$app->response->cookies->add(new \yii\web\Cookie([
+                    'name' => $this->storageKey,
+                    'value' => $language,
+                ]));
+                break;
+            
+            default:
+                return false;
+                break;
+        }
+    }
+}
